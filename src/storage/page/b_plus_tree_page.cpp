847d88f8cace0a03634f37127653da88bfa99f61
YTKO-OKTY <tkyoun03@naver.com>
2025-05-16 14:08:14 +0900

    Implementation_Final 250516

--- src/storage/page/b_plus_tree_page.cpp
+++ src/storage/page/b_plus_tree_page.cpp
@@ -17,23 +17,23 @@ namespace bustub {
  * Helper methods to get/set page type
  * Page type enum class is defined in b_plus_tree_page.h
  */
-bool BPlusTreePage::IsLeafPage() const { return false; }
-bool BPlusTreePage::IsRootPage() const { return false; }
-void BPlusTreePage::SetPageType(IndexPageType page_type) {}
+bool BPlusTreePage::IsLeafPage() const { return page_type_ == IndexPageType::LEAF_PAGE; }
+bool BPlusTreePage::IsRootPage() const { return parent_page_id_ == INVALID_PAGE_ID; }
+void BPlusTreePage::SetPageType(IndexPageType page_type) {page_type_ = page_type;}
 
 /*
  * Helper methods to get/set size (number of key/value pairs stored in that
  * page)
  */
-int BPlusTreePage::GetSize() const { return 0; }
-void BPlusTreePage::SetSize(int size) {}
-void BPlusTreePage::IncreaseSize(int amount) {}
+int BPlusTreePage::GetSize() const { return size_; }
+void BPlusTreePage::SetSize(int size) { size_ = size; }
+void BPlusTreePage::IncreaseSize(int amount) { size_ += amount; }
 
 /*
  * Helper methods to get/set max size (capacity) of the page
  */
-int BPlusTreePage::GetMaxSize() const { return 0; }
-void BPlusTreePage::SetMaxSize(int size) {}
+int BPlusTreePage::GetMaxSize() const { return max_size_; }
+void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
 
 /*
  * Helper method to get min page size
